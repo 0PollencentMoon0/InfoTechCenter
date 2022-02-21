ff4cf66070075ab9a8a2c5e970f9e1c8da944d5f
Charlotte Osburn <charlottejosburn@hotmail.com>
2022-02-21 13:42:33 -0500

    Rearranged Libraries and added heading with version

--- MergedBranches.py
+++ MergedBranches.py
@@ -1,19 +1,23 @@
-# WELCOME BRANCH
-
-# Code Name - Hornet
+# Programmer: Charlotte Osburn
+# Date 2.21.2022
+# Version 3.6
 
 from time import sleep  #Print to one line with time delay between prints
 import colorama
 from colorama import Fore, Back, Style
+# Libraries Imported Here
 colorama.init(strip=False, autoreset=True)
 
-print(Fore.LIGHTMAGENTA_EX + Back.BLACK + Style.BRIGHT + "Welcome to Hornets InfoTechCenter\n"); sleep(1)
+import random
 
-print(Fore.LIGHTCYAN_EX + Back.WHITE + Style.DIM + "Hornet's Operating System Booting Up\n")
+# WELCOME BRANCH
+# Code Name - Hornet
+
+print(Fore.LIGHTMAGENTA_EX + "Welcome to Hornets InfoTechCenter\n"); sleep(1)
+
+print(Fore.LIGHTCYAN_EX + "Hornet's Operating System Booting Up\n")
 
 # GAS BRANCH
-
-import random
 
 # Gas Level Function
 def gasLevelGauge():
